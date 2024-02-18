d014906ac6f4e30eac0dd5da137d64c687c50023
faisal-hdev <itsfaisalchowdhury@gmail.com>
2024-02-18 15:15:30 +0600

    Common function added

--- js-flie/app.js
+++ js-flie/app.js
@@ -1,17 +1,31 @@
 const allSeatsBtn = document.getElementsByClassName("seat-add");
 
 let count = 0;
-// let seatLeftCount = 1;
 for (const singleSeatBtn of allSeatsBtn) {
-  singleSeatBtn.addEventListener("click", function (e) {
-    const seatLeftEl = document.getElementById("seat-left").innerText;
-    const convertedSeatLeft = parseInt(seatLeftEl);
-    const leftValue = convertedSeatLeft - 1;
+  singleSeatBtn.addEventListener("click", function (event) {
+    const seatEl = event.target;
+    seatEl.classList.remove("bg-[#F7F8F8]");
+    seatEl.classList.add("bg-[#1DD100]");
 
+    const seatLeftEl = getElementTextById("seat-left");
+    const leftValue = seatLeftEl - 1;
     count = count + 1;
+
+    // const seatNumber = event.target.innerText;
+    // const economoyEl = getElementTextById("economoy");
+    // const seatPriceEl = getElementTextById("seat-price");
+    // const convertedSeatPriceEl = parseInt(seatPriceEl);
+    // console.log(seatNumber, economoyEl, convertedSeatPriceEl);
+
     setInnerText("seat-count", count);
-    setInnerText("seat-left", leftValue);
+    setInnerText("seat-left", parseInt(leftValue));
   });
+}
+
+function getElementTextById(elementId) {
+  const element = document.getElementById(elementId);
+  const text = element.innerText;
+  return text;
 }
 
 function setInnerText(elementId, value) {
