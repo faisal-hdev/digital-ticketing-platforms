57936d37baafe97389b26aafa4ad9349efd151f9
faisal-hdev <itsfaisalchowdhury@gmail.com>
2024-02-19 03:35:46 +0600

    Responsive design are added

--- js-flie/app.js
+++ js-flie/app.js
@@ -6,48 +6,48 @@ for (const singleSeatBtn of allSeatsBtn) {
   singleSeatBtn.addEventListener("click", function (event) {
     if (count >= 4) {
       return alert("You have already selected four seat. you can't add more");
+    } else {
+      const seatEl = event.target;
+      seatEl.classList.remove("bg-[#F7F8F8]");
+      seatEl.classList.add("bg-[#1DD100]");
+      seatEl.classList.add("text-white");
+
+      const seatLeftEl = getElementTextById("seat-left");
+      const leftValue = seatLeftEl - 1;
+      count = count + 1;
+
+      const seatNumber = event.target.innerText;
+
+      const selectContainer = document.getElementById("select-container");
+      const li = document.createElement("li");
+      li.classList.add("flex");
+      li.classList.add("justify-between");
+      li.classList.add("mb-2");
+
+      const p = document.createElement("p");
+      p.innerText = seatNumber;
+      const p2 = document.createElement("p");
+      p2.innerText = "Economoy";
+      const p3 = document.createElement("p");
+      p3.innerText = 550;
+
+      const price = p3.innerText;
+      const priceConvert = parseInt(price);
+
+      totalPrice = totalPrice + priceConvert;
+      setInnerText("total-price", totalPrice);
+
+      const gTotalEl = (document.getElementById("grand-total").innerText =
+        totalPrice);
+
+      li.appendChild(p);
+      li.appendChild(p2);
+      li.appendChild(p3);
+      selectContainer.appendChild(li);
+
+      setInnerText("seat-count", count);
+      setInnerText("seat-left", parseInt(leftValue));
     }
-
-    const seatEl = event.target;
-    seatEl.classList.remove("bg-[#F7F8F8]");
-    seatEl.classList.add("bg-[#1DD100]");
-    seatEl.classList.add("text-white");
-
-    const seatLeftEl = getElementTextById("seat-left");
-    const leftValue = seatLeftEl - 1;
-    count = count + 1;
-
-    const seatNumber = event.target.innerText;
-
-    const selectContainer = document.getElementById("select-container");
-    const li = document.createElement("li");
-    li.classList.add("flex");
-    li.classList.add("justify-between");
-    li.classList.add("mb-2");
-
-    const p = document.createElement("p");
-    p.innerText = seatNumber;
-    const p2 = document.createElement("p");
-    p2.innerText = "Economoy";
-    const p3 = document.createElement("p");
-    p3.innerText = 550;
-
-    const price = p3.innerText;
-    const priceConvert = parseInt(price);
-
-    totalPrice = totalPrice + priceConvert;
-    setInnerText("total-price", totalPrice);
-
-    const gTotalEl = (document.getElementById("grand-total").innerText =
-      totalPrice);
-
-    li.appendChild(p);
-    li.appendChild(p2);
-    li.appendChild(p3);
-    selectContainer.appendChild(li);
-
-    setInnerText("seat-count", count);
-    setInnerText("seat-left", parseInt(leftValue));
   });
 }
 
@@ -67,6 +67,7 @@ function grandTotal() {
 
   const gradTotalCost = getElementTextById("grand-total");
   const convertGrandTotal = parseInt(gradTotalCost);
+  couponInput.value = "";
 
   if (couponInput === "NEW15") {
     const fiftyPercentDiscount = (convertGrandTotal * 15) / 100;
