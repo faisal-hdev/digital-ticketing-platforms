6b06004b5ec0e31582c85520d59186e0b5ebe280
faisal-hdev <itsfaisalchowdhury@gmail.com>
2024-02-18 18:58:31 +0600

    Total price calculate and grand total show money featured added

--- js-flie/app.js
+++ js-flie/app.js
@@ -1,6 +1,8 @@
 const allSeatsBtn = document.getElementsByClassName("seat-add");
 
 let count = 0;
+let totalPrice = 0;
+let grandTotal = 0;
 for (const singleSeatBtn of allSeatsBtn) {
   singleSeatBtn.addEventListener("click", function (event) {
     const seatEl = event.target;
@@ -11,11 +13,34 @@ for (const singleSeatBtn of allSeatsBtn) {
     const leftValue = seatLeftEl - 1;
     count = count + 1;
 
-    // const seatNumber = event.target.innerText;
-    // const economoyEl = getElementTextById("economoy");
-    // const seatPriceEl = getElementTextById("seat-price");
-    // const convertedSeatPriceEl = parseInt(seatPriceEl);
-    // console.log(seatNumber, economoyEl, convertedSeatPriceEl);
+    const seatNumber = event.target.innerText;
+
+    const selectContainer = document.getElementById("select-container");
+    const li = document.createElement("li");
+    li.classList.add("flex");
+    li.classList.add("justify-between");
+    li.classList.add("mb-2");
+
+    const p = document.createElement("p");
+    p.innerText = seatNumber;
+    const p2 = document.createElement("p");
+    p2.innerText = "Economoy";
+    const p3 = document.createElement("p");
+    p3.innerText = 550;
+
+    const price = p3.innerText;
+    const priceConvert = parseInt(price);
+
+    totalPrice = totalPrice + priceConvert;
+    setInnerText("total-price", totalPrice);
+
+    const gTotalEl = (document.getElementById("grand-total").innerText =
+      totalPrice);
+
+    li.appendChild(p);
+    li.appendChild(p2);
+    li.appendChild(p3);
+    selectContainer.appendChild(li);
 
     setInnerText("seat-count", count);
     setInnerText("seat-left", parseInt(leftValue));
